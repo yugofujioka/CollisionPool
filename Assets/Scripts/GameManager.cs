da652839b3b1b0866ec5cbd11694aa7f328f30ff
Fujioko <fujioka@tanoshimasu.com>
2018-09-24 12:19:28 +0900

    ファイル追加

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static CollisionPool collision;
+
+    void Awake() {
+        GameManager.collision = new CollisionPool();
+        GameManager.collision.Initialize();
+
+        // 1m = 1pixの位置にカメラを移動させる
+        Camera cam = Camera.main;
+        float tanFOV = Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        float dist = Screen.height * 0.5f / tanFOV;
+        cam.transform.localPosition = new Vector3(0f, 0f, -dist);
+    }
+
+    void Update() {
+        float elapsedTime = Time.deltaTime;
+
+        // コリジョンの更新（判定）
+        GameManager.collision.Proc(elapsedTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+    Collision col = null;
+
+    void Start() {
+        // コリジョンの呼び出し
+        this.col = GameManager.collision.PickOut(COL_CATEGORY.PLAYER);
+        this.col.range = 50f;
+        this.col.hitHandler = this.HitCallback;
+        
+        // スクリーン座標をコリジョンに通知
+        Vector3 scPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        this.col.point = scPos;
+    }
+
+    void Update() {
+        // スクリーン座標をコリジョンに通知
+        Vector3 scPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        this.col.point = scPos;
+    }
+    
+    /// <summary>
+    /// 接触コールバック
+    /// </summary>
+    /// <param name="atk">影響を与えるコリジョン</param>
+    /// <param name="def">影響を受けるコリジョン</param>
+    private void HitCallback(Collision atk, Collision def) {
+        // atkに自身、defに相手（この場合は敵）が受け渡される
+        Debug.Log("PLAYER HIT !!!");
+
+        atk.enable = false;    // 自身のコリジョンの返却
+    }
+}
